cb3181dde6be6510b18a77caa0540b645c4bea81
myrandagoestospace <myranda.uselton@gmail.com>
2021-08-04 21:07:32 +0000

    attempt to create graph

--- metadata_parse.py
+++ metadata_parse.py
@@ -1,11 +1,23 @@
 # Metadata parsing
 # Description:
 
+from owslib.csw import CatalogueServiceWeb
 import kglab
-import rdflib
 
 def main():
 
-
+    #csw = CatalogueServiceWeb("https://emodnet.ec.europa.eu/geonetwork/emodnet/eng/csw?service=CSW&request=GetCapabilities&VERSION=2.0.2")
+    #print(csw.identification.type)
+    
+    kg = kglab.KnowledgeGraph().load_rdf("small_metadata.xml", format="xml")
+    
+    
+    subgraph = kglab.SubgraphTensor(kg)
+    pyvis_graph = subgraph.build_pyvis_graph()
+    
+    pyvis_graph.force_atlas_2based()
+    pyvis_graph.show("tmp.fig03.html")
 
 main()
+    
+    
